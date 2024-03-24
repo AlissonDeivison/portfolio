fa4d434bcb2ce7b472fd1867a32ebe1afceff0ca
Alisson Deivison S Pereira <uepbalisson@gmail.com>
2024-03-24 13:42:45 -0300

    Created header component to render project logo and nav menu bar

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ContainerComponent } from '../container/container.component';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
