6ecb28981fb5799c70d3b35e2e94c3b709297540
Alisson Deivison S Pereira <uepbalisson@gmail.com>
2024-03-24 17:44:19 -0300

    Adds import of the CardComponent component into the HeaderComponent

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../container/container.component';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ContainerComponent],
+  imports: [ContainerComponent, CardComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
