125caee37c13d12c7533b41f17caf881638d1f87
Alisson Deivison S Pereira <uepbalisson@gmail.com>
2024-03-24 13:49:03 -0300

    Added component imports in the AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./shared/footer/footer.component";
+import { HeaderComponent } from "./shared/header/header.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [
+      RouterOutlet, 
+      FooterComponent, 
+      HeaderComponent,
+    ]
 })
 export class AppComponent {
   title = 'portfolio';
